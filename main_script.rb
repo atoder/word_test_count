db3750048f3f0e87460269ccee745990ede6ada6
atoder <alexandert@gmail.com>
2015-05-17 20:29:59 -0700

    Moved out files to lib and spec

--- main_script.rb
+++ main_script.rb
@@ -1,10 +1,18 @@
-require './word_sequence.rb'
+require_relative 'lib/word_sequence.rb'
 
+file_name = ARGV[0].to_s
+
+if file_name.nil? || file_name.empty?
+  #if empty, use default sample.txt
+  file_name = "sample.txt"
+end
+
+exit()
 
 word_sequence = WordSequence.new
 
 # Step 1 - Read the file and get array of all words
-word_sequence.read_file_and_generate_words
+word_sequence.read_file_and_generate_words(file_name)
 
 # Step 2 - Create sequences for each word
 word_sequence.generate_sequence
